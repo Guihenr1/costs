806d8367ed94fee8905cb44add5cb96fa474028f
Guilherme <guilherme.pomp@gmail.com>
2022-06-22 23:54:45 -0300

    Home components

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,27 @@
+import Input from "../form/Input";
+import Select from "../form/Select";
+import SubmitButton from "../form/SubmitButton";
+import styles from "./ProjectForm.module.css";
+
+function ProjectForm({ btnText }) {
+  return (
+    <form className={styles.form}>
+      <Input
+        type="text"
+        text="Project name"
+        name="name"
+        placeholder="Enter the project name"
+      />
+      <Input
+        type="number"
+        text="Project budget"
+        name="budget"
+        placeholder="Enter the project budget"
+      />
+      <Select name="category_id" text="Select the category" />
+      <SubmitButton text={btnText} />
+    </form>
+  );
+}
+
+export default ProjectForm;
